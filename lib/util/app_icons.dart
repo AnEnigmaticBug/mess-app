d902c8ce47650c30da6b570f14a89b9674463462
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-11-14 16:20:57 +0530

    Add the app's icon font

---
+++ lib/util/app_icons.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const _fontFamily = 'AppIcons';
+
+  static const profile_outlined = IconData(0x61, fontFamily: _fontFamily);
+  static const profile_solid = IconData(0x62, fontFamily: _fontFamily);
+  static const grubs_outlined = IconData(0x63, fontFamily: _fontFamily);
+  static const grubs_solid = IconData(0x64, fontFamily: _fontFamily);
+  static const menu_outlined = IconData(0x65, fontFamily: _fontFamily);
+  static const menu_solid = IconData(0x66, fontFamily: _fontFamily);
+  static const feedback_outlined = IconData(0x67, fontFamily: _fontFamily);
+  static const feedback_solid = IconData(0x68, fontFamily: _fontFamily);
+  static const more_outlined = IconData(0x69, fontFamily: _fontFamily);
+  static const more_solid = IconData(0x6A, fontFamily: _fontFamily);
+  static const thumbs_up_outlined = IconData(0x6B, fontFamily: _fontFamily);
+  static const thumbs_up_solid = IconData(0x6C, fontFamily: _fontFamily);
+  static const thumbs_down_outlined = IconData(0x6D, fontFamily: _fontFamily);
+  static const thumbs_down_solid = IconData(0x6E, fontFamily: _fontFamily);
+}
