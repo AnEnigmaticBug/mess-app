eee7cb27a896264673105ef852e242d68890c4d2
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-11-18 19:31:40 +0530

    Add a file to hold colors used globally in the app

---
+++ lib/util/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const appBarBackground = Color(0xFFE8DECC);
+  static const backgroundGradient1 = Color(0xFFE8DECC);
+  static const backgroundGradient2 = Color(0xFFE1DAD4);
+  static const bottomNavBackground = Color(0xFFF8F6F1);
+  static const bottomNavSelected = Color(0xFF766B6B);
+  static const bottomNavUnselected = Color(0xFFBFB0A3);
+  static const textDark = Color(0xFF6B6154);
+}
