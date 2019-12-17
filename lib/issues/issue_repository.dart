d2720186a6bd0cd3f25642bfe74588567051bd12
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-12-17 15:19:00 +0530

    Add code for retrieving issues from backend

--- lib/issues/issue_repository.dart
+++ lib/issues/issue_repository.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:messapp/issues/issue.dart';
+import 'package:messapp/util/date.dart';
+import 'package:messapp/util/http_exceptions.dart';
 import 'package:meta/meta.dart';
 import 'package:nice/nice.dart';
 import 'package:sqflite/sqlite_api.dart';
@@ -15,6 +19,80 @@ class IssueRepository {
   List<ActiveIssue> _activeIssueCache = [];
   List<SolvedIssue> _solvedIssueCache = [];
 
+  Future<List<ActiveIssue>> get activeIssues async {
+    if (_activeIssueCache.isEmpty) {
+      await _populateCaches();
+    }
+
+    return _activeIssueCache;
+  }
+
+  Future<List<SolvedIssue>> get solvedIssues async {
+    if (_solvedIssueCache.isEmpty) {
+      await _populateCaches();
+    }
+
+    return _solvedIssueCache;
+  }
+
+  Future<void> refresh() async {
+    final res = await _client.get('/issues');
+
+    if (res.statusCode != 200) {
+      throw res.toException();
+    }
+
+    final issuesJson = json.decode(res.body);
+    final activeIssuesJson = issuesJson['active'];
+    final solvedIssuesJson = issuesJson['solved'];
+
+    await _db.transaction((txn) async {
+      await txn.rawDelete('''
+        DELETE
+          FROM ActiveIssue
+      ''');
+
+      await txn.rawDelete('''
+        DELETE
+          FROM SolvedIssue
+      ''');
+
+      for (var issueJson in activeIssuesJson) {
+        await txn.rawInsert('''
+          INSERT
+            INTO ActiveIssue (id, title, dateCreated, upvoteCount, upvoted, flagged)
+          VALUES (?, ?, ?, ?, ?, ?)
+        ''', [
+          issueJson['id'],
+          issueJson['title'],
+          issueJson['date_created'],
+          issueJson['upvote_count'],
+          issueJson['upvoted'] ? 1 : 0,
+          issueJson['flagged'] ? 1 : 0,
+        ]);
+      }
+
+      for (var issueJson in solvedIssuesJson) {
+        await txn.rawInsert('''
+          INSERT
+            INTO SolvedIssue (id, title, dateCreated, upvoteCount, upvoted, flagged, dateSolved, reason)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', [
+          issueJson['id'],
+          issueJson['title'],
+          issueJson['date_created'],
+          issueJson['upvote_count'],
+          issueJson['upvoted'] ? 1 : 0,
+          issueJson['flagged'] ? 1 : 0,
+          issueJson['date_solved'],
+          issueJson['reason'],
+        ]);
+      }
+    });
+
+    await _populateCaches();
+  }
+
   Future<void> setUpvoted({
     @required int issueId,
     @required bool value,
@@ -24,4 +102,53 @@ class IssueRepository {
     @required int issueId,
     @required bool value,
   }) async {}
+
+  Future<void> _populateCaches() async {
+    await _populateActiveIssueCache();
+    await _populateSolvedIssueCache();
+  }
+
+  Future<void> _populateActiveIssueCache() async {
+    _activeIssueCache.clear();
+
+    final rows = await _db.rawQuery('''
+      SELECT id, title, dateCreated, upvoteCount, upvoted, flagged
+        FROM ActiveIssue
+    ''');
+
+    for (var row in rows) {
+      _activeIssueCache.add(ActiveIssue(
+        id: row['id'],
+        title: row['title'],
+        dateCreated: Date.parse(row['dateCreated']),
+        upvoteCount: row['upvoteCount'],
+        upvoted: row['upvoted'] == 1,
+        flagged: row['flagged'] == 1,
+        repository: this,
+      ));
+    }
+  }
+
+  Future<void> _populateSolvedIssueCache() async {
+    _solvedIssueCache.clear();
+
+    final rows = await _db.rawQuery('''
+      SELECT id, title, dateCreated, upvoteCount, upvoted, flagged, dateSolved, reason
+        FROM SolvedIssue
+    ''');
+
+    for (var row in rows) {
+      _solvedIssueCache.add(SolvedIssue(
+        id: row['id'],
+        title: row['title'],
+        dateCreated: Date.parse(row['dateCreated']),
+        upvoteCount: row['upvoteCount'],
+        upvoted: row['upvoted'] == 1,
+        flagged: row['flagged'] == 1,
+        dateSolved: Date.parse(row['dateSolved']),
+        reason: row['reason'],
+        repository: this,
+      ));
+    }
+  }
 }
