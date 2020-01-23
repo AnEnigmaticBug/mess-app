136b7f685065d61b79897e50ebbb7260264ab403
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2020-01-24 01:49:54 +0530

    Add pull to refresh to notices

--- lib/notice/notice_screen.dart
+++ lib/notice/notice_screen.dart
@@ -7,6 +7,7 @@ import 'package:messapp/util/simple_presenter.dart';
 import 'package:messapp/util/ui_state.dart';
 import 'package:messapp/util/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:messapp/util/extensions.dart';
 
 class NoticeScreen extends StatelessWidget {
   const NoticeScreen({
@@ -36,61 +37,71 @@ class NoticeScreen extends StatelessWidget {
               );
             }
 
-            return ListView.builder(
-              padding: const EdgeInsets.all(12.0),
-              itemCount: state.data.length,
-              itemBuilder: (context, position) {
-                return Container(
-                  padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppColors.shadow,
-                        offset: Offset(3.0, 8.0),
-                        blurRadius: 10.0,
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            state.data[position].heading,
-                            style: TextStyle(
-                                fontFamily: 'Quicksand',
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16.0,
-                                color: AppColors.textDark),
-                            maxLines: 2,
-                          ),
-                          SizedBox(
-                            height: 12.0,
-                          ),
-                          Text(
-                            state.data[position].startDate,
-                            style: TextStyle(
-                                fontFamily: 'Quicksand',
-                                fontSize: 12.0,
-                                color: AppColors.textDark),
-                          )
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        children: <Widget>[
-                          _criticalIcon(state.data[position].isCritical)
-                        ],
-                      ),
-                      SizedBox(
-                        width: 8.0,
-                      )
-                    ],
-                  ),
-                );
+            return RefreshIndicator(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(12.0),
+                itemCount: state.data.length,
+                itemBuilder: (context, position) {
+                  return Container(
+                    padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.shadow,
+                          offset: Offset(3.0, 8.0),
+                          blurRadius: 10.0,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              state.data[position].heading,
+                              style: TextStyle(
+                                  fontFamily: 'Quicksand',
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0,
+                                  color: AppColors.textDark),
+                              maxLines: 2,
+                            ),
+                            SizedBox(
+                              height: 12.0,
+                            ),
+                            Text(
+                              state.data[position].startDate,
+                              style: TextStyle(
+                                  fontFamily: 'Quicksand',
+                                  fontSize: 12.0,
+                                  color: AppColors.textDark),
+                            )
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          children: <Widget>[
+                            _criticalIcon(state.data[position].isCritical)
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8.0,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              onRefresh: () async {
+                try{
+                  final presenter = Provider.of<SimplePresenter<NoticeRepository, List<Notice>>>(context);
+                  await presenter.refresh();
+                }on Exception catch (e) {
+                  e.toString().showSnackBar(context);
+                }
               },
             );
           }
